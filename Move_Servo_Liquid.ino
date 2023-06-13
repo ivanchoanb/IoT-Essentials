d9284fee6d07b555bbb6f9e845b4fbd471117b6f
Ivan Nocua <ivanchoa96@gmail.com>
2023-06-13 18:31:08 -0500

    Complemento de movimiento con servos

--- Move_Servo_Liquid.ino
+++ Move_Servo_Liquid.ino
@@ -1,4 +1,4 @@
-# include <Liquid_Crystal.h>
+# include <LiquidCrystal.h>
 # include <Servo.h>
 
 LiquidCrystal lcd(8,9,4,5,6,7);
@@ -10,8 +10,8 @@ void setup(){
   lcd.begin(16,2);
 }
 
-coid loop(){
-  lcd.setCurso(0,0);
+void loop(){
+  lcd.setCursor(0,0);
   lcd.print("electrotec,pe");
   lcd.setCursor(0,1);
   int valor = analogRead(tecla);
@@ -23,4 +23,31 @@ coid loop(){
     lcd.clear();
     }
     else if (valor >405 && valor <415)
+    {
+      myservo.write(144);
+      lcd.print ("LEFT");
+      delay(1000);
+      lcd.clear();
+    }
+    else if (valor > 250 && valor < 260)
+    {
+      myservo.write(108);
+      lcd.print ("DOWN");
+      delay(1000);
+      lcd.clear();
+    }
+    else if (valor > 95 && valor < 105)
+    {
+      myservo.write(72);
+      lcd.print ("UP");
+      delay(1000);
+      lcd.clear();
+    }
+    else if (valor == 0)
+    {
+      myservo.write(36);
+      lcd.print ("RIGHT");
+      delay(1000);
+      lcd.clear();
+    }
   }
